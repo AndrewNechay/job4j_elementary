1c71772a65dc3a24ec7bfabf32226271b6c73b5f
Buzlya <polarich34@gmail.com>
2022-04-15 14:56:04 +0300

    !

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Andrey Nechaev");
+        System.out.println("01.05.1995");
+    }
+}
