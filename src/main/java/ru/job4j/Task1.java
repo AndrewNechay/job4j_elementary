d56bb669ff22aa8fc73ddb7bd3bbc4a02815c33d
Buzlya <polarich34@gmail.com>
2022-04-17 15:13:52 +0300

    Задача. Определите тип данных для переменных. [#172634]

---
+++ src/main/java/ru/job4j/Task1.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        byte age = 27;     //тип байт потому как выше не имет смысла, будет тяжелее файл
+        System.out.println(age);
+        char gender = 'M';   // тип данных "символ"
+        System.out.println(gender);
+        short height = (175); // т.к. тип "байт" доходит максимум до 127 байтов
+        System.out.println(height);
+        int brick = (40000); // т.к. на 100 к.м. дома надо около 40 тысяч кирпичей, а тип данных "шорт" располагает
+        // максимум на 32 тысячи
+        System.out.println(brick);
+        int bacteria = 10000000; // 10 в 7 степени = 10 миллионов бактерий
+        System.out.println(bacteria);
+        int spaceobjects = (149597870); // единици измерения в астрономии примерно равна среднему
+        // расстоянию земли от солнца - 149597870 км
+        System.out.println(spaceobjects);
+        char jobPermission = 'Y'; // разрешение на работу- да
+        System.out.println(jobPermission);
+        short filesize = (1494); // размер этого файла - 1439 байт, "байт" не подойдёт уже
+        System.out.println(filesize);
+
+    }
+}
