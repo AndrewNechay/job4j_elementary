99744ac3e079a401a5827adb7b7d72fc8c1eed4f
Buzlya <polarich34@gmail.com>
2022-05-15 21:38:19 +0300

    4.6.1. Switch [#334070 #301174]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -8,6 +8,21 @@ public class SwitchWeek {
                 name = "Понедельник";
                 break;
             case 2:
+                name = "Вторник";
+                break;
+            case 3:
+                name = "Среда";
+                break;
+            case 4:
+                name = "Четверг";
+                break;
+            case 5:
+                name = "Пятница";
+                break;
+            case 6:
+                name = "Суббота";
+                break;
+            case 7:
                 name = "Воскресенье";
                 break;
             default:
@@ -22,6 +37,11 @@ public class SwitchWeek {
         System.out.println(nameOfDay(1));
         System.out.println(nameOfDay(2));
         System.out.println(nameOfDay(3));
+        System.out.println(nameOfDay(4));
+        System.out.println(nameOfDay(5));
+        System.out.println(nameOfDay(6));
+        System.out.println(nameOfDay(7));
+        System.out.println(nameOfDay(8));
     }
 
 }
